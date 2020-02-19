52d2e96ee1c9babb9f2000b4fa7f5d83165b4156
johnlime <thelimejuiceproductions@gmail.com>
2020-02-19 20:18:14 +0900

    Kohonen's SOM main.py. Cannot cast subclass to its private base class

--- Unit Neurons/main.cpp
+++ Unit Neurons/main.cpp
@@ -9,12 +9,26 @@
 #include <iostream>
 #include "input_output.hpp"
 #include "kohonen_som.hpp"
+#define SAMPLES 100
 
 int main(int argc, const char * argv[]) {
     // define input neurons
-    
+    float x_memory [SAMPLES];
+    float y_memory [SAMPLES];
+    for (int i = 0; i < SAMPLES; i++){
+        x_memory[i] = rand() % SAMPLES;
+        y_memory[i] = rand() % SAMPLES;
+    }
+    FloatInputNeuron io_neuron [2]
+    {
+        {x_memory},
+        {y_memory}
+    };
     // define mapping neurons
-    
+    FloatMappingNeuron* maps [25];
+    for (int i = 0; i < 25; i++){
+        maps[i] = FloatMappingNeuron(io_neuron);
+    }
         // assign neighboring neurons
     
     // define global operator
