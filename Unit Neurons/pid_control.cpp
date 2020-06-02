fb29d847588d33669ef9b9af5ac63a79a94d86d4
johnlime <thelimejuiceproductions@gmail.com>
2020-06-02 20:51:07 +0900

    adding pid controller framework

---
+++ Unit Neurons/pid_control.cpp
@@ -0,0 +1,9 @@
+//
+//  pid_control.cpp
+//  Unit Neurons
+//
+//  Created by John Lime on 2020/06/02.
+//  Copyright © 2020 Mioto Takahashi. All rights reserved.
+//
+
+#include "pid_control.hpp"
---
+++ Unit Neurons/pid_control.hpp
@@ -0,0 +1,14 @@
+//
+//  pid_control.hpp
+//  Unit Neurons
+//
+//  Created by John Lime on 2020/06/02.
+//  Copyright © 2020 Mioto Takahashi. All rights reserved.
+//
+
+#ifndef pid_control_hpp
+#define pid_control_hpp
+
+#include <stdio.h>
+
+#endif /* pid_control_hpp */
